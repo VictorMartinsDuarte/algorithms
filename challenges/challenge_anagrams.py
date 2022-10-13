3e0db0b8a79dba16aca2623197be79a2385ef7dd
Victor Martins <victor.martins.duarte@gmail.com>
2022-10-13 17:03:18 -0300

    Feat: Req-01 Adds logic to function is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
+    if len(first_string) != len(second_string):
+        return False
+    first = first_string.lower()
+    second = second_string.lower()
+    for letter in first:
+        if letter in second:
+            second = second.replace(letter, "", 1)
+        else:
+            return False
+    return True
