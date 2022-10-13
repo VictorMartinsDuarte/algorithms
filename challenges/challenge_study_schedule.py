0ced72d9c350a7c661c79fcfa2b30c65bfcce0b5
Victor Martins <victor.martins.duarte@gmail.com>
2022-10-13 18:12:03 -0300

    Feat: Req-03 Adds logic to function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    try:
+        students_count = 0
+        for time in permanence_period:
+            if time[0] <= target_time <= time[1]:
+                students_count += 1
+    except TypeError:
+        return None
+    return students_count
